ac7ca52e3635f9153250a092fe43063c045828e8
Eero Vilpponen <eero.vilpponen@gmail.com>
2024-10-04 18:19:35 +0300

    Proof of concept

---
+++ script.js
@@ -0,0 +1,108 @@
+
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+let webhook = urlParams.get('webhook');
+
+if (!webhook) {
+    webhook = prompt("Enter your webhook URL");
+    // Add the webhook to the URL (first encode it)
+    window.location.href = window.location.href + "?webhook=" + encodeURIComponent(webhook);
+}
+
+
+window.DATA = [];
+window.SENTDATA = 0;
+
+
+const setSentData = function(newSentData) {
+    window.SENTDATA = newSentData;
+    const sentDataEl = document.getElementById("sent-data");
+    if (!sentDataEl) {
+        console.error("Could not find sent data element");
+    }
+    sentDataEl.innerHTML = newSentData;
+}
+
+
+
+const updateDataSending = function(newStatus) {
+    const dataSendingStatusEl = document.getElementById("data-send-status");
+    if (!dataSendingStatusEl) {
+        console.error("Could not find data sending status element");
+    }
+    if (newStatus == "sending") {
+        dataSendingStatusEl.innerHTML = "Sending data...";
+    }
+    else if (newStatus == "idle") {
+        dataSendingStatusEl.innerHTML = "Idle";
+    }
+    else {
+        console.error("Invalid data sending status");
+        dataSendingStatusEl.innerHTML = "Error";
+    }
+}
+        
+
+
+const sendData = async function(data) {
+    const webhookURL = new URL(webhook);
+    for (const [key, value] of Object.entries(data)) {
+        webhookURL.searchParams.append(key, value);
+    }
+    return fetch(webhookURL, {
+        mode: 'no-cors',
+        method: 'GET',
+    });
+};
+
+const getNextDataToSend = function() {
+    for (let i = 0; i < window.DATA.length; i++) {
+        if (!window.DATA[i].sent) {
+            return window.DATA[i];
+        }
+    }
+    return null;
+}
+
+let sendDataTimeout;
+
+const tryToSendData = async function() {
+    const data = getNextDataToSend();
+    if (data) {
+        updateDataSending("sending");
+        try {
+            await sendData(data);
+            updateDataSending("idle");
+            setSentData(window.SENTDATA + 1);
+            data.sent = true;
+            sendDataTimeout = setTimeout(tryToSendData, 10);
+        } catch (error) {
+            console.error("Error sending data", error);
+            updateDataSending("error");
+            sendDataTimeout = setTimeout(tryToSendData, 1000);
+        }
+    } else {
+        updateDataSending("idle");
+        sendDataTimeout = setTimeout(tryToSendData, 1000);
+    }
+}
+
+
+sendDataTimeout = setTimeout(tryToSendData, 1000);
+
+
+
+const testButton = document.createElement("button");
+testButton.innerHTML = "Test";
+testButton.onclick = function() {
+    window.DATA.push({
+        "time": (new Date()).toISOString(),
+        "data": Math.random(),
+        "content": "Test",
+    });
+};
+
+document.body.appendChild(testButton);
+
